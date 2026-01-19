60b623fa182c18fa6f914394d200a4fce9bd36e7
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-01-19 12:25:29 +0000

    Try something else

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#git status
-#if git status --porcelain | grep -q '^??'; then
-#  echo "Untracked files found. Staging and committing them."
-#  git config --global user.email 181243999+tna-da-bot@users.noreply.github.com
-#  git config --global user.name tna-da-bot
-#  git add -A
-#  git commit -m "Generate signature files"
-#  git push origin $(git rev-parse --abbrev-ref HEAD)
-#else
-#  echo "No untracked files found. Nothing to commit."
-#fi
-echo $(git rev-parse --abbrev-ref HEAD)
+git status
+if git status --porcelain | grep -q '^??'; then
+  echo "Untracked files found. Staging and committing them."
+  git config --global user.email 181243999+tna-da-bot@users.noreply.github.com
+  git config --global user.name tna-da-bot
+  git add -A
+  git commit -m "Generate signature files"
+  git push origin HEAD:$GITHUB_BASE_REF
+else
+  echo "No untracked files found. Nothing to commit."
+fi
